e2bcb12b89a5b5fd14ba6926ae24ed08c803a762
Naveed <46922100+nedanwr@users.noreply.github.com>
2022-08-10 15:43:53 +0500

    Feat: init distube

--- src/struct/Client.ts
+++ src/struct/Client.ts
@@ -5,6 +5,8 @@ import {
     Partials
 } from "discord.js";
 import { ConfigManager } from "../managers/config";
+import DisTube from "distube";
+import { YtDlpPlugin } from "@distube/yt-dlp";
 
 export class Rythm extends Client {
     commands = new Collection<string, any>();
@@ -12,6 +14,7 @@ export class Rythm extends Client {
 
     public readonly config: ConfigManager;
     public readonly prefix: string;
+    public readonly distube: DisTube;
 
     public constructor() {
         super({
@@ -46,5 +49,14 @@ export class Rythm extends Client {
         this.commands = new Collection();
         this.config = new ConfigManager(this);
         this.prefix = this.config.botSettings().prefix;
+        this.distube = new DisTube(this, {
+            leaveOnStop: false,
+            emitNewSongOnly: true,
+            emitAddSongWhenCreatingQueue: false,
+            emitAddListWhenCreatingQueue: false,
+            plugins: [
+                new YtDlpPlugin(),
+            ],
+        });
     }
 }
