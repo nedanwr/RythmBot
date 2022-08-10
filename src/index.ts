2b6ae8a06b6e38b8636091a8af06be3f9240e364
Naveed <46922100+nedanwr@users.noreply.github.com>
2022-08-10 13:17:00 +0500

    Feat: call loadEvents function after login

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import { Rythm } from "./struct/Client";
+import { loadEvents } from "./utils/eventLoader";
 
 const client: Rythm = new Rythm();
 
 // TODO: use top level await if we switch to ESM
 void client
-    .login(client.config.botSettings().token);
+    .login(client.config.botSettings().token)
+    .then(async () => {
+        await loadEvents(client);
+    });
