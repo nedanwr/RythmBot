252204733b91b9a8f8e151e8a50cab1b11bd6698
Naveed <46922100+nedanwr@users.noreply.github.com>
2022-08-10 13:17:25 +0500

    Feat: create ready event

---
+++ src/events/ready.ts
@@ -0,0 +1,19 @@
+import { Rythm } from "../struct/Client";
+import { logger } from "../logger";
+
+module.exports = {
+    name: "ready",
+    once: true,
+    async run(client: Rythm) {
+        logger.info(`${client.user?.username} is ready to play awesome music!`);
+
+        try {
+            await client.user?.setPresence({
+                status: client.config.presenceSettings().status,
+            });
+        }
+        catch (err: any | unknown) {
+            logger.error(`Failed to set startup presence: ${err.message}`);
+        }
+    }
+}
