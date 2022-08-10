a94df990f160cac57ac0cdfbbf346ddaf7d50790
Naveed <46922100+nedanwr@users.noreply.github.com>
2022-08-10 12:43:54 +0500

    Feat: create config manager

---
+++ src/managers/config.ts
@@ -0,0 +1,27 @@
+import type { PresenceStatusData } from "discord.js";
+import { Manager } from ".";
+const config = require("../../config.json");
+
+interface BotSettings {
+    token: string;
+    prefix: string;
+}
+
+interface PresenceSettings {
+    status: PresenceStatusData | undefined;
+    message: PresenceStatusData | undefined;
+    type: PresenceStatusData | undefined;
+}
+
+export class ConfigManager extends Manager {
+    botSettings = (): BotSettings => ({
+        token: config.token,
+        prefix: config.prefix,
+    });
+
+    presenceSettings = (): PresenceSettings => ({
+        status: config.presence.status,
+        message: config.presence.activity.message,
+        type: config.presence.activity.type,
+    });
+}
