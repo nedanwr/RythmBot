679c41a8087aaa8e6694166135fcd1eb4fdf0eea
Naveed <46922100+nedanwr@users.noreply.github.com>
2022-08-10 14:48:27 +0500

    Refactor: changes to conform to old event loader

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,19 +1,15 @@
 import { Rythm } from "../struct/Client";
 import { logger } from "../logger";
 
-module.exports = {
-    name: "ready",
-    once: true,
-    async run(client: Rythm) {
-        logger.info(`${client.user?.username} is ready to play awesome music!`);
+module.exports = async (client: Rythm) => {
+    logger.info(`${client.user?.username} is ready to play awesome music!`);
 
-        try {
-            await client.user?.setPresence({
-                status: client.config.presenceSettings().status,
-            });
-        }
-        catch (err: any | unknown) {
-            logger.error(`Failed to set startup presence: ${err.message}`);
-        }
+    try {
+        await client.user?.setPresence({
+            status: client.config.presenceSettings().status,
+        });
+    }
+    catch (err: any | unknown) {
+        logger.error(`Failed to set startup presence: ${err.message}`);
     }
 }
